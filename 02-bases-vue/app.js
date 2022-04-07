7be69865c511a07fe6bdb8e8038e13dde0af6fc9
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-07 07:57:17 -0300

    eventos - cambiar dom por methods this

--- 02-bases-vue/app.js
+++ 02-bases-vue/app.js
@@ -11,6 +11,16 @@ const app = Vue.createApp({
             quote: 'Perro que ladra no muerde',
             author: 'Martin Fierro'
         }
+    },
+    methods: {
+        changeQuote(event) {
+            console.log(event)
+            this.author = 'Benjamin Correa'
+            this.capitalize()
+        },
+        capitalize() {
+            this.quote = this.quote.toUpperCase()
+        }
     }
 })
 
