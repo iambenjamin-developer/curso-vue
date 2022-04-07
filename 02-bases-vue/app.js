b0b27b983f9aa8e6762f50f8078d2f9bea15ac57
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-07 08:06:09 -0300

    limpiar

--- 02-bases-vue/app.js
+++ 02-bases-vue/app.js
@@ -1,27 +1,14 @@
 console.log(Vue)
 
 const app = Vue.createApp({
-    // template: `
-    // <h1>Hola mundo</h1>
-    // <p> {{ !true ? 'Activo' : 'Inactivo'}}</p>
-    // <p>desde app.js</p>
-    // `
+
     data() {
         return {
             quote: 'Perro que ladra no muerde',
             author: 'Martin Fierro'
         }
     },
-    methods: {
-        changeQuote(event) {
-            console.log(event)
-            this.author = 'Benjamin Correa'
-            this.capitalize()
-        },
-        capitalize() {
-            this.quote = this.quote.toUpperCase()
-        }
-    }
+
 })
 
 app.mount('#myApp')
