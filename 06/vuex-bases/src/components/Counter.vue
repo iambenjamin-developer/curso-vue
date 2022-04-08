3825923cd722ba40a9e259a609b034469a3aa3fb
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-04-08 08:59:21 -0300

    mutaciones

--- 06/vuex-bases/src/components/Counter.vue
+++ 06/vuex-bases/src/components/Counter.vue
@@ -1,11 +1,25 @@
 <template>
-  
-  <h1>Counter Vuex</h1>
-  <h2>Acceso directo {{$store.state.count}}</h2>
+    <h1>Counter Vuex</h1>
+    <h2>Acceso directo {{ $store.state.count }}</h2>
+
+    <button @click="incrementarEnUno">+1</button>
+    <button @click="incrementarEnCinco">+5</button>
 </template>
 
 <script>
+
 export default {
+    methods:
+    {
+        incrementarEnUno() {
+
+            this.$store.commit('incrementOne')
+        },
+        incrementarEnCinco() {
+
+            this.$store.commit('incrementFive')
+        },
+    }
 
 }
 </script>
--- 06/vuex-bases/src/store/index.js
+++ 06/vuex-bases/src/store/index.js
@@ -2,11 +2,23 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    count: 1
+    count: 1,
+    lastMutation: 'Ninguna'
   },
   getters: {
   },
   mutations: {
+    incrementOne(state) {
+      state.count++
+      state.lastMutation = 'incrementOne'
+
+    },
+    incrementFive(state) {
+      state.count += 5
+      state.lastMutation = 'incrementFive'
+
+    },
+
   },
   actions: {
   },
