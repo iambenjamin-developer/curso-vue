3fdc93e71a60a7ed19060561550238f295adc509
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-05-06 08:26:45 -0300

    computed vs methods

--- 04/fundamentos/src/components/Counter.vue
+++ 04/fundamentos/src/components/Counter.vue
@@ -2,6 +2,19 @@
 
     <h2>Counter!!</h2>
     <p> {{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
+
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
 
 </template>
 
@@ -11,6 +24,18 @@ export default {
     data() {
         return {
             counter: 5
+        }
+    },
+    methods: {
+        getSquareValue() {
+            console.log("method => getSquareValue")
+            return this.counter * this.counter
+        }
+    },
+    computed: {
+        getSquareCounterValue() {
+            console.log("computed => getSquareCounterValue")
+            return this.counter * this.counter
         }
     }
 }
