6c1eefd6524a0eece184428d6377f63856e11c1d
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-05-06 08:49:31 -0300

    incrementar y decrementar

--- 04/fundamentos/src/components/Counter.vue
+++ 04/fundamentos/src/components/Counter.vue
@@ -2,19 +2,11 @@
 
     <h2>Counter!!</h2>
     <p> {{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareValue() }}</p>
 
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
-    <p> {{ counter }} <sup>2</sup> = {{ getSquareCounterValue }}</p>
+    <div>
+        <button v-on:click="increase">+1</button>
+        <button @click="decrease">-1</button>
+    </div>
 
 </template>
 
@@ -30,6 +22,12 @@ export default {
         getSquareValue() {
             console.log("method => getSquareValue")
             return this.counter * this.counter
+        },
+        increase() {
+            this.counter++;
+        },
+        decrease() {
+            this.counter--;
         }
     },
     computed: {
@@ -42,4 +40,19 @@ export default {
 </script>
 
 <style>
+button {
+    background-color: #64BB87;
+    border-radius: 5px;
+    border: 1px solid white;
+    color: white;
+    cursor: pointer;
+    margin: 0 5px;
+    padding: 5px 15px;
+    transition: 0.3s ease-in-out;
+}
+
+button:hover {
+    background-color: #003b19;
+    transition: 0.3s ease-in-out;
+}
 </style>
