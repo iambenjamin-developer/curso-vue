424e2198afb01d9b698b3d63c6003ee2a77ea081
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-05-09 10:09:29 -0300

    primera prueba

--- 04/fundamentos/tests/unit/example.spec.js
+++ 04/fundamentos/tests/unit/example.spec.js
@@ -3,17 +3,13 @@ describe('Example Component', () => {
   test('Debe de ser mayor a 10', () => {
 
     //Arrange
-    let value = 15;
+    let value = 10;
 
     //Act
     value = value + 2;
 
     //Assert
-    if (value > 10) {
-      // todo bien
-    } else {
-      throw `${value} no es mayor a 10`
-    }
+    expect(value).toBeGreaterThan(10);
   })
 
 })
