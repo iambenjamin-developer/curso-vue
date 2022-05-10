4d113c5d564530077ad7a292a7111734c156b388
Benjamin Correa <iambenjamin.developer@gmail.com>
2022-05-10 07:25:25 -0300

    jest verificar valor de una etiqueta HTML

--- 04/fundamentos/tests/unit/components/counter.spec.js
+++ 04/fundamentos/tests/unit/components/counter.spec.js
@@ -3,10 +3,25 @@ import Counter from '@/components/Counter'
 
 describe('Counter Component', () => {
 
-    test('debe hacer match con el snapshot', () => {
+    // test('debe hacer match con el snapshot', () => {
+
+    //     const wrapper = shallowMount(Counter);
+
+    //     expect(wrapper.html()).toMatchSnapshot();
+    // })
+
+    test('h2 debe de tener el valor por defecto "Titulo por defecto!"', () => {
 
         const wrapper = shallowMount(Counter);
 
-        expect(wrapper.html()).toMatchSnapshot();
+
+        expect(wrapper.find('h2').exists()).toBeTruthy();
+
+        // expect(wrapper.find('h20').exists()).toBe(true);
+
+        const h2Value = wrapper.find('h2').text();
+
+        expect(h2Value).toBe("Titulo por defecto!")
+
     })
 })
