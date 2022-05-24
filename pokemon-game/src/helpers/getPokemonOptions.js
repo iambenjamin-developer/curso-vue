0ff19e3dcef3c011ecf27bfe432f597756bcd570
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-05-24 10:56:07 -0300

    return pokemon random

--- pokemon-game/src/helpers/getPokemonOptions.js
+++ pokemon-game/src/helpers/getPokemonOptions.js
@@ -1,4 +1,5 @@
 import pokemonApi from "@/api/pokemonApi";
+import { CLOSING } from "ws";
 
 const getPokemons = () => {
 
@@ -6,18 +7,34 @@ const getPokemons = () => {
 
     return pokemonArr.map((_, index) => index + 1)
 }
-const getPokemonsOptions = () => {
+const getPokemonsOptions = async () => {
 
     const mixPokemons = getPokemons().sort(() => Math.random() - 0.5)
-    getPokemonNames(mixPokemons.splice(0, 4));
+    const pokemons = await getPokemonNames(mixPokemons.splice(0, 4));
+    console.table(pokemons);
+
+    return pokemons
 }
 
 const getPokemonNames = async ([a, b, c, d] = []) => {
 
-    const response = await pokemonApi.get(`/2`)
-    console.log(a, b, c, d);
-    console.log(response.data.id, response.data.name)
-  
+    // const response = await pokemonApi.get(`/2`)
+    // console.log(a, b, c, d);
+    // console.log(response.data.id, response.data.name)
 
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ]
+    const [pokemon1, pokemon2, pokemon3, pokemon4] = await Promise.all(promiseArr)
+
+    return [
+        { id: pokemon1.data.id, name: pokemon1.data.name },
+        { id: pokemon2.data.id, name: pokemon2.data.name },
+        { id: pokemon3.data.id, name: pokemon3.data.name },
+        { id: pokemon4.data.id, name: pokemon4.data.name },
+    ]
 }
 export default getPokemonsOptions;
