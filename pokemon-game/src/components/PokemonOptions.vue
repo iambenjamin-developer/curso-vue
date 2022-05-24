7fe9d6775f66a9e375e41239aad209c0d896a334
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-05-24 08:56:19 -0300

    agregar componentes pokemon

---
+++ pokemon-game/src/components/PokemonOptions.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3>Pokemon options</h3>
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+  name: "PokemonOptions",
+  components: {
+    PokemonOptions
+    ,
+  },
+};
+</script>
+
+<style>
+</style>
