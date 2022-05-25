6453d50ba68d41d8bcb08ab4ac651b597d85df04
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-05-25 15:11:19 -0300

    diferencia entre lazy loading y importacion directa

--- rutas-ciclo/src/router/router.js
+++ rutas-ciclo/src/router/router.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import * as VueRouter from 'vue-router'
 
-import AboutPage from '@/modules/pokemon/pages/AboutPage'
+// import AboutPage from '@/modules/pokemon/pages/AboutPage'
 import ListPage from '@/modules/pokemon/pages/ListPage'
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage'
 import NotFoundPage from '@/modules/shared/pages/NotFoundPage'
 
 const routes = [
-    { path: '/', component: ListPage },
-    { path: '/about', component: AboutPage },
-    { path: '/id', component: PokemonPage },
-    { path: '/:pathMatch(.*)*', component: NotFoundPage },
+    {
+        path: '/',
+        component: ListPage
+    },
+    {
+        path: '/about',
+        component: () => import(/*webpackChunkName: "AboutPage"*/ '@/modules/pokemon/pages/AboutPage')
+        // component: AboutPage
+    },
+    {
+        path: '/id',
+        component: PokemonPage
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: NotFoundPage
+    },
 ]
 
 const router = createRouter({
