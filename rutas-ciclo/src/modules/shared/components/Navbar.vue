15969a0e45416ce73377dbc22541bb1ed62ba080
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-05-25 15:44:45 -0300

    cargar en easy loading el navbar

---
+++ rutas-ciclo/src/modules/shared/components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a href="/">Pokemon List</a>
+    <a href="/id">Pokemon por Id</a>
+    <a href="/about">About</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped >
+</style>
