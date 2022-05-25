a267acebf0159b5e7e14298cefa65e176a4dbd2b
Benjamin Correa <benjamin.c@smartwaystudio.com>
2022-05-25 14:56:14 -0300

    NotFound Component

---
+++ rutas-ciclo/src/modules/shared/pages/NotFoundPage.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>Not Found 404</h1>
+</template>
+
--- rutas-ciclo/src/router/router.js
+++ rutas-ciclo/src/router/router.js
@@ -4,11 +4,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import AboutPage from '@/modules/pokemon/pages/AboutPage'
 import ListPage from '@/modules/pokemon/pages/ListPage'
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage'
+import NotFoundPage from '@/modules/shared/pages/NotFoundPage'
 
 const routes = [
     { path: '/', component: ListPage },
     { path: '/about', component: AboutPage },
     { path: '/id', component: PokemonPage },
+    { path: '/:pathMatch(.*)*', component: NotFoundPage },
 ]
 
 const router = createRouter({
